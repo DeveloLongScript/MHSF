d541c0b6ed5dd185667736e65f282cbd2187feb1
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2024-09-06 20:31:22 -0500

    feat: alert when not in production

--- src/config/banner.tsx
+++ src/config/banner.tsx
@@ -1,7 +1,23 @@
-import { Server } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 /** used when there is a outage */
 export const banner = {
-  isBanner: false,
-  bannerText: <></>,
+  isBanner:
+    process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
+      ? true
+      : /** Set this to true when outage --->*/ false,
+  bannerText:
+    process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" ? (
+      <>
+        Your not in production!{" "}
+        <Link href="https://list.mlnehut.com">
+          <Button variant="link" className="dark:text-black">
+            Go to production
+          </Button>
+        </Link>
+      </>
+    ) : (
+      <>{/** Set this to an explanation! */}</>
+    ),
 };
