032a4b67f10ad2b0b5d976b90ed079123c9b701b
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2025-03-01 19:48:15 -0600

    fix(www): 404 null error #7

---
+++ apps/www/next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.clerk.com",
+			},
+		],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/docs",
+				destination: "/docs/getting-started",
+				permanent: true,
+			},
+		];
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	output: "standalone",
+	experimental: {
+		// Disable static error pages generation
+		staticPageGenerationTimeout: 0,
+	},
+};
+
+export default nextConfig;
